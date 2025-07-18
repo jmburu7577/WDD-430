00905c866d099c1a4cfdf752a06010fe80fd1d60
James Mburu Maina <jmaina@knh.or.ke>
2025-07-18 05:25:01 +0300

    m

---
+++ react-todo-list/src/App.js
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (!inputValue.trim()) {
+      alert('Please enter a to-do item');
+      return;
+    }
+    setTodos([...todos, { id: Date.now(), text: inputValue, completed: false }]);
+    setInputValue('');
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Add a to-do"
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodo(todo.id)}
+            />
+            <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
